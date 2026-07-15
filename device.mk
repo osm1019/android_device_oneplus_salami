255914ad9b7704ce92136020fbf37c6e46e9cbee
Oscar <osm1019@gmail.com>
2026-07-14 19:01:55 -0700

    salami: tidy camera inherit comment
    Change-Id: I4c2c3e4f23223ac84f6b8a23e63c303bc4840217

--- device.mk
+++ device.mk
@@ -103,7 +103,5 @@ $(call inherit-product, device/oneplus/sm8550-common/common.mk)
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/salami/salami-vendor.mk)
 
-# OPlus camera (salami OOS16 app-layer: app / gallery / SDK / product libs / system_ext OPlus libs).
-# odm camera algo libs come from vendor/oneplus/salami (device tree). Scratch repo, isolated from
-# the sm8750 vendor/oplus/camera. Promote camera-salami -> camera once fully validated.
+#camera
 $(call inherit-product-if-exists, vendor/oplus/camera-salami/opluscamera.mk)
